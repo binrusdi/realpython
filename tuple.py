fdda9b7bec8b0ec2702431f3c073b25d67568631
binrusdi <mrusdiana07@gmail.com>
2025-11-08 15:51:53 +0000

    penjelas  tuple beres

--- tuple.py
+++ tuple.py
@@ -18,14 +18,34 @@ x = ('yaris', 'alpard')
 y = ('datsun',)
 
 x += y
-print(x)
+print(x) # ('yaris', 'alpard', 'datsun')
 
 contoh1 = (1,2,3)
-print(contoh1)
+print(contoh1) # (1, 2, 3)
 
 del contoh1
 
 try:
     print(contoh1)
 except NameError:
-    print('Nama variable tidak ada')
+    print('Nama variable tidak ada') # Nama variable tidak ada, karena contoh1 sudah di hapus oleh key "del"
+    
+# Modifikasi tuple, menggunakan 'list'
+daftar_belanja = ('Sayur', 'Buah', 'Furnitur')
+y = list(daftar_belanja)
+y.append('Termos')
+daftar_belanja = tuple(y)
+print(daftar_belanja, type(daftar_belanja))
+
+# Method tuple
+deret_angka = (1, 9, 8, 5, 6, 8, 9)
+print(deret_angka.index(5)) # output: 3
+'''
+method index -> mencari index nilai tertentu lalu di kembalikan no index nya
+'''
+
+print(deret_angka.count(9)) # output 2
+'''
+method count -> mencari berapa kali nilai tertentu muncul di dalam sebuah tuple.
+mengembalikan total kemunculan
+'''
